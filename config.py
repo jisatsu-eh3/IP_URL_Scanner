bc1c29b5908df4bba4f787fe0ec649fcd7776bb0
jisatsu-eh3 <77258274+jisatsu-eh3@users.noreply.github.com>
2021-05-27 01:36:09 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-vt_api_key = "b39cbb28b6107000b3d496045d09aabe73f488203db366ceb3824b2b46b831c8"
+#Virus Total API
+vt_api_key = ""
 
-hb_api_key = 'o8fm0ye8f82c6038ukbnsznn4bc3af863w0v62sy3d500469o6a5z8eeb4d92707'
-hb_secret = 'bef400d8ddb546592fdef7b55b768db8e330622ce04d4ce1'
-
-md_api_key = "18db898365f240c985c19661f173df7c"
+#MetaDefender API 
+md_api_key = ""
